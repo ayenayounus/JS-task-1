0092c5234b2728dcc4730c2f42e4f18c90f6a008
Ayena <ayenayounus56@gmail.com>
2023-04-09 21:18:33 +0500

    Task done

---
+++ app.js
@@ -0,0 +1,8 @@
+
+var zakaat_percentage = 0.025 ;
+var user_input = prompt(" enter amount to calculate zakaat: ");
+var result = zakaat_percentage*user_input; 
+
+console.log(result);
+
+alert("Your zakaat is: "+ result);
